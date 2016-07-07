e6f3d8c4a2c46bff6f17f145d930b1236cb6c61d
Carlin Au <carlinau@gmail.com>
2016-07-07 09:09:45 -0400

    splash page, basic route setup

---
+++ app.js
@@ -0,0 +1,15 @@
+// Dependencies
+const express = require('express');
+const routes  = require('./app/routes/routes.js');
+const morgan  = require('morgan');
+
+// Set up Application
+const app = express();
+var port = process.env.port || 8080;
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/views"));
+routes(app);
+
+// Launch
+app.listen(port);
---
+++ app/routes/routes.js
@@ -0,0 +1,9 @@
+module.exports = RouteHandler;
+
+function RouteHandler(app)
+{
+  app.get('/', function(req, res)
+  {
+    res.sendfile("views/index.html");
+  });
+}
