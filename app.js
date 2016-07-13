51c4ebe7fde8a6c22e5b6055c89c8b07c20ff979
Carlin Au <carlinau@gmail.com>
2016-07-13 00:30:51 -0400

    MyMLH is now working

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // Dependencies
-const express = require('express');
-const morgan  = require('morgan');
-const routes  = require('./app/routes/routes.js');
-const config  = require('./config/config.js');
+const express    = require('express');
+const morgan     = require('morgan');
+const bodyParser = require('body-parser')
+const routes     = require('./app/routes/routes.js');
+const config     = require('./config/config.js');
 
 // Set up Application
 const app = express();
 var port = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/views"));
-routes(app);
+routes(app, config);
 
 // Launch
 app.listen(port);
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,9 +1,45 @@
+const request = require('request');
 module.exports = RouteHandler;
 
 function RouteHandler(app, config)
 {
-  app.get('/', function(req, res)
+  app.get('/', (req, res)=>
   {
-    res.sendfile("views/index.html");
+    res.sendfile('views/index.html');
+  });
+
+  app.get('/test', (req, res)=>
+  {
+    res.sendfile('views/test.html');
+    console.log(config.MYMLH_CLIENT_ID);
+  });
+
+  app.get('/dashboard/account', (req, res)=>
+  {
+    let AUTH_CODE = req.query.code;
+    console.log(req.query.code);
+    request({
+      url: 'https://my.mlh.io/oauth/token',
+      method: 'POST',
+      qs: {
+        client_id: config.MYMLH_CLIENT_ID,
+        client_secret: config.MYMLH_SECRET,
+        code: AUTH_CODE,
+        redirect_uri: 'http://localhost:8080/mymlh/callback',
+        grant_type: 'authorization_code'
+      }
+    }, (error, response, body)=>{
+      if(error) {
+        return console.log(error);
+      }
+      //console.log(response);
+      console.log()
+      console.log(response);
+    });
+  });
+
+  app.get('/dashboard', (req, res)=>
+  {
+    res.sendfile('views/dashboard.html');
   });
 }
