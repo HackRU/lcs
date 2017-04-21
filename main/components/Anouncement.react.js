d926a57b43418986a2a37cfbe95d72c459ed4672
jandeancatarata <jandeancatarata@users.noreply.github.com>
2017-04-21 09:11:12 -0400

    Pull for Dashboard (#4)
    * Dashboard v 0.0.0.1
    
    * updated README
    
    * updated README
    
    * Now displays 'anouncements'
    
    * Finished slackmsg/anouncements push notifications
    
    * Added user authentication hookup and display profile data
    
    * split Anouncement text into two span's of text and time, one with respective class's 'anouncement-text' and 'anouncement-time'
    
    * added loading of QR code image but, does not do error handling for when an emails qr code has not been created yet
    
    * added errorhandling of qrcode images
    
    * cleaning up
    
    * cleaning up pt2
    
    * Added gavel functionality
    
    * Updated README
    
    * hooking up Dashboard to Camelot
    
    * merged everything

---
+++ main/components/Anouncement.react.js
@@ -0,0 +1,63 @@
+var React = require('react');
+
+function normalize(time){
+  var meridiem = "am";
+  var colon = time.indexOf(':');
+  var hours =  parseInt(time.slice(0,colon));
+  if(hours >= 12){
+    hours = hours % 12;
+    meridiem = "pm";
+  }
+  if(hours == 0) hours = 12;
+  return hours + time.substring(colon)+meridiem;
+}
+
+function cleanupTags(text){
+  var ret = text + "";
+  var indexlt = ret.indexOf('<');
+  ret = ret.replace('<!channel>','');
+  ret = ret.replace('<!everyone>','');
+  while(indexlt != -1){
+    var indexrt = ret.indexOf('>');
+    if(ret.charAt(indexlt+1) == '@'){
+      var slice = ret.slice(indexlt,indexrt+1);
+      ret = ret.replace(slice,'');
+    }
+    var left = '';
+    var middle = '';
+    var right = '';
+    if(indexlt > 0) left = ret.substring(0,indexlt);
+    middle = ret.slice(indexlt+1,indexrt);
+    if(indexrt < indexrt-1) right = ret.substring(indexrt);
+    ret = left+middle+right;
+    indexlt = ret.indexOf('<');
+  }
+  ret = ret.replace('&gt','>');
+  ret = ret.replace('&lt','<');
+  return ret;
+}
+
+function cleanup( anouncement){
+  var end = anouncement.ts.indexOf('.');
+  var unix = parseInt(anouncement.ts.substring(0,end));
+  var date = new Date(unix *1000);
+   
+  var hours = date.getHours();
+  var minutes = '0' + date.getMinutes();
+  
+  return normalize(hours + ':' + minutes.substr(-2)) + '.';
+}
+
+
+module.exports = Anouncement= React.createClass({
+  render: function(){
+    var anouncement = this.props.anouncement;
+    return (
+      <div className={'anouncement' + ' active'}>
+        <span className='anouncement-time'> {cleanup(anouncement)}</span>
+        <span className='anouncement-text'> {cleanupTags(anouncement.text)} </span>
+        <div className='short-separator'></div>
+      </div>
+    )
+  }
+});
