65edd9a4ddc02b0773465c65f030c49835d77f00
Sail338 <srihari.shankar1@gmail.com>
2017-12-27 14:49:31 -0500

    make headers clear

--- authorize.py
+++ authorize.py
@@ -1,6 +1,7 @@
 import string
 import random
 import json
+import pymongo
 def authorize(event,context):
     if('email' not in event  or 'password' not in event):
         return ({"statusCode":400,"body":"Invalid Request"})
@@ -13,7 +14,7 @@ def authorize(event,context):
     #generate auth token
     token = gen()
     bod_ = {"authtoken":token}
-    ret_val = { "statusCode":200,"isBase64Encoded": False, "headers": { "test":"test" },"body" :json.dumps(bod_)}
+    ret_val = { "statusCode":200,"isBase64Encoded": False, "headers": { "Content-Type":"application/json" },"body" :json.dumps(bod_)}
     return ret_val
 def gen (size = 20, chars = string.ascii_lowercase + string.digits + string.ascii_uppercase):
     return ''.join(random.choice(chars) for _ in range(size))
