12311f9ce94edaf23cb0b1f1e9d52d1d6c033397
Sail338 <srihari.shankar1@gmail.com>
2017-12-27 12:39:19 -0500

    add authtoken

--- authorize.py
+++ authorize.py
@@ -1,7 +1,16 @@
+import string
+import random
 def authorize(event,context):
+
+
+
     email = event['email']
     hash_ = event['hash']
     #querydb 
     
     #generate auth token
-    token = ""
+    token = gen()
+    ret_val = { 'authtoken' : token }
+    return ret_val
+def gen (size = 20, chars = string.ascii_lowercase + string.digits + string.ascii_uppercase):
+    return ''.join(random.choice(chars) for _ in range(size))
