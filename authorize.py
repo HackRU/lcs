1398f6a4047591233fb73a1be0e5531807163ccc
Sail338 <srihari.shankar1@gmail.com>
2017-12-30 12:54:46 -0500

    fixed bugs

--- authorize.py
+++ authorize.py
@@ -1,8 +1,10 @@
 import string
 import random
 import json
+import pymongo
 from pymongo import MongoClient
 import config
+print(pymongo.version)
 def authorize(event,context):
 
     if('email' not in event  or 'password' not in event):
@@ -10,7 +12,13 @@ def authorize(event,context):
     email = event['email']
     pass_ = event['password']
     client = MongoClient(config.DB_URI)
-     
+    db = client['camelot-test']
+    db.authenticate(config.DB_USER,config.DB_PASS)
+    tests = db['test']
+
+    dat = tests.find_one({"email":event['email'], "password":event['password']})
+    if dat == None or dat == [] or dat == ():
+        return ({"statusCode":403,"body":"invalid email,hash combo"})
     #querydb 
     
     #generate auth token
@@ -20,4 +28,5 @@ def authorize(event,context):
     return ret_val
 def gen (size = 20, chars = string.ascii_lowercase + string.digits + string.ascii_uppercase):
     return ''.join(random.choice(chars) for _ in range(size))
+
 
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 source bin/activate
 pip install -r requirements.txt
 mkdir deploy/
-cp lib/python3.6/site-packages/* deploy/
+cp -r lib/python3.6/site-packages/ deploy/
 cp $1 deploy/
 cp config.py deploy/
 cd deploy/
-zip dep.zip *
+zip -r dep.zip *
+cd ..
+cp deploy/dep.zip .
+rm -rf deploy/
