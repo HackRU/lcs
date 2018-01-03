4a4a230901430d68bf6d5c058f893829a241b7bd
Heman <hemangandhi@gmail.com>
2018-01-03 14:49:43 -0500

    no u' in python strings

--- authorize.py
+++ authorize.py
@@ -48,19 +48,19 @@ def mlh_callback(event, context):
     else:
         params['code'] = event['queryStringParameters']['code']
         access_tok_json = requests.post(MLH_TOK_BASE_URL, params=params).json()
-        access_token = access_tok_json[u'access_token']
-        scopes = access_tok_json[u'scope']
+        access_token = access_tok_json['access_token']
+        scopes = access_tok_json['scope']
 
     mlh_user = requests.get(MLH_USER_BASE_URL, params={'access_token': access_token}).json()
 
-    if mlh_user[u'status'] != 'OK':
+    if mlh_user['status'] != 'OK':
         return ({"statusCode":400,"body":"MLH Troubles!"})
 
     client = MongoClient(config.DB_URI)
     db = client['camelot-test']
     db.authenticate(config.DB_USER,config.DB_PASS)
     test = db['test']
-    user = test.find_one({'email': mlh_user[u'data'][u'email']})
+    user = test.find_one({'email': mlh_user['data']['email']})
     if user == None or user == [] or user == ():
         #making new user here
         event['user_data'] = mlh_user['data']
