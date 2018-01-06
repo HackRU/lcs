f23749b410707aeaa1e8736b0a2d2a659d215a48
Heman <hemangandhi@gmail.com>
2018-01-05 22:19:54 -0500

    tester being tested

--- tester.py
+++ tester.py
@@ -4,30 +4,35 @@ import json
 def test(url):
     user_email = "team@nonruhackathon.notemail.com"
     passhash = 42
-    auth = requests.post(url + '/authorize', json=json.dumps(usr_dict))
-    print("Non-existant: ", auth.body)
+    usr_dict = {'email': user_email, 'password': passhash}
+    auth = requests.post(url + '/authorize', json=(usr_dict))
+    print("Non-existant: ", auth.text)
 
     user_email = "hemangandhi@gmail.com"
-    auth = requests.post(url + '/authorize', json=json.dumps(usr_dict))
-    print("Bad password: ", auth.body)
+    usr_dict = {'email': user_email, 'password': passhash}
+    auth = requests.post(url + '/authorize', json=(usr_dict))
+    print("Bad password: ", auth.text)
 
     passhash = 12345
     usr_dict = {'email': user_email, 'password': passhash}
-    auth = requests.post(url + '/authorize', json=json.dumps(usr_dict))
-    token = auth.json()['authtoken']
+    auth = requests.post(url + '/authorize', json=(usr_dict))
+    token = auth.json().get('authtoken')
     print("Got token:", token)
+    if token is None:
+        print("Bad token")
+        return
 
     val_dict = {'email': user_email, 'authtoken': token}
-    valid = requests.get(url + '/validate', json=json.dumps(val_dict))
-    print("From validate:", valid.body)
+    valid = requests.get(url + '/validate', json=(val_dict))
+    print("From validate:", valid.text)
 
     rando = "the.scrub@rutgers.edu"
     val_dict = {'user_email': rando, 'authtoken': token, 'auth_email': user_email}
-    valid = requests.get(url + '/update', json=json.dumps(val_dict))
-    print("From update:", valid.body)
+    valid = requests.get(url + '/update', json=(val_dict))
+    print("From update:", valid.text)
 
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     if len(argv) < 2:
         print("USAGE:", argv[0], "<URL of the lambdas>")
     else:
