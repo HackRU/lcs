3b09ed5254c59e86376166ed434f89f9eb7d6735
Hari Prasad R. Amoor <hra25@ls.cs.rutgers.edu>
2018-01-17 17:19:54 -0500

    fixed read.py

--- read.py
+++ read.py
@@ -16,8 +16,12 @@ def read_info(event, context):
     tests = db['test']
 
     if context['role'] == 'director' or context['role'] == 'organizer':
-        return list(tests.aggregate(event['query']))
+        return list(tests.aggregate(event['query'])) if event['aggregate'] else tests.find(event['query'])
 
     else:
-        return tests.find_one(event['query'])
-
+	restricted_fields = ['mlhid', 'email', 'first_name', 'last_name', 'date_of_birth', 'email', 'password', 'id', 'github', 'resume', 'short_answer', 'data_sharing', 'rules_and_conditions']
+	
+	res_ = tests.aggregate(event['query']) if event['aggregate'] else tests.find(event['query'])
+	del doc[abstracted_data] if abstracted_data in doc for abstracted_data in restricted_fields for doc in res_ # Heman, I sincerely hope that you waste twenty minutes trying to understand what this line does. - Hari 
+	 
+	return res_
