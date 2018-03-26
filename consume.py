0efe422a9566f0f09c6b2aba9a79da44b2769597
Sail338 <srihari.shankar1@gmail.com>
2018-03-26 14:52:17 -0400

    fixes

---
+++ consume.py
@@ -0,0 +1,68 @@
+import random
+from pymongo import MongoClient
+import validate
+import config 
+import string
+from datetime import datetime, timedelta
+import hashlib
+
+def updateUserFromMagicLink(userCollection,maglinkobj,event):
+    """
+        Updates User Based on a magic link
+    """
+    #if the user forgot, then read the password and change it
+    if maglinkobj['forgot'] == True:
+        pass_ = event['password']
+        pass_ = hashlib.md5(pass_.encode('utf-8') ).hexdigest() 
+        checkifmlh = userCollection.find_one({"email":maglinkobj['email']})
+        if checkifmlh['mlh']:
+            return config.add_cors_headers({"statusCode":400,"body":"Something went wrong, you are probbally an MLH user change password using MLH"})
+        userCollection.update_one({"email":maglinkobj['email']},{'$set':{'password':pass_}})
+        return config.add_cors_headers({"statusCode":200,"body":"Sucessfully updated your password"})
+    #else we want an auth token and an email
+    else:
+        ret_ = validate.validate(event,None)
+        if ret_['statusCode'] == 200:
+           #Grab the permissions object 
+            permissions = maglinkobj['permissions']
+            for i in permissions:
+                if i == 'director':
+                    userCollection.update_one({'email':event['email'] }, {'$set':{'role.director':True}})
+                elif i == 'judge':
+                    userCollection.update_one({'email':event['email'] }, {'$set':{'role.judge':True}})
+                elif i == 'organizer':
+                    userCollection.update_one({'email':event['email'] }, {'$set':{'role.organizer':True}})
+                elif i == 'volunteer':
+                    userCollection.update_one({'email':event['email'] }, {'$set':{'role.volunteer':True}})
+                elif i == 'mentor':
+                    userCollection.update_one({'email':event['email'] }, {'$set':{'role.mentor':True}})
+
+            return config.add_cors_headers({"statusCode":200,"body":"Sucessfully updated your role"})
+                    
+                    
+
+
+
+
+
+
+def consumeUrl(event,context):
+    """
+        Lambda function to consume a url. Queries the database and checks permissions and updates accordingly
+    """
+    if 'link' not in event:
+        return config.add_cors_headers({"statusCode":400,"body":"No magic link provided"})
+
+    client = MongoClient(config.DB_URI)
+    db = client['lcs-db']
+    db.authenticate(config.DB_USER,config.DB_PASS)
+    #user collection
+    tests = db['test']
+    #maglink collection
+    magiclinks = db['magiclinks']
+    maglinkobj = magiclinks.find_one({"link":event['link']}) 
+    if maglinkobj:
+        statusCode = updateUserFromMagicLink(tests,maglinkobj,event)
+        #remove link after consuming
+        magiclinks.remove({"link":maglinkobj['link']})
+        return statusCode
