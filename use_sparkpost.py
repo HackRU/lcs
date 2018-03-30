a7896b4e78fbd1e30b7eab05439028fce6f6cf1b
Heman <hemangandhi@gmail.com>
2018-03-30 00:25:14 -0400

    uses read to query for emails + docs

--- use_sparkpost.py
+++ use_sparkpost.py
@@ -1,11 +1,20 @@
 from sparkpost import SparkPost
 from validate import get_validated_user
 import config
+from read import read_info
 from pymongo import MongoClient
 
 emails = SparkPost(config.SPARKPOST_KEY)
 
 def list_all_templates(event, context):
+    """
+    Gets a list of all the templates iff the
+    user (email, token keys in event) is an
+    authenticated director.
+
+    The template list is some sort of dictionary
+    from sparkpost.
+    """
     val, usr = get_validated_user(event, context)
     if not val or not user['role']['director']:
         return config.add_cors_headers({'statusCode': 400, 'body': usr})
@@ -14,6 +23,16 @@ def list_all_templates(event, context):
         return config.add_cors_headers({'statusCode': 400, 'body': templs})
 
 def send_to_emails(event, context):
+    """
+    If the user (validated via the email and token keys)
+    is a non-director, the only keys allowed are the 'recipient' one
+    and the 'template' one. (The template should be a valid template.)
+
+    If a valid director is logged in then the 'recipient' list can be
+    arbitrary. If a 'query' is provided and no 'recipient' list is, then
+    the 'query' is run as if passed to the 'read' endpoint
+    the email key of every returned document is emailed.
+    """
     if 'template' not in event:
         return config.add_cors_headers({'statusCode': 400, 'body': 'Missing template'})
     if 'recipients' not in event and 'query' not in event:
@@ -26,15 +45,12 @@ def send_to_emails(event, context):
         return config.add_cors_headers({'statusCode': 400, 'body': "Not authorized to send emails!"})
     else:
         if 'query' in event and 'recipients' not in event:
-            client = MongoClient(config.DB_URI)
-            db = client['lcs-db']
-            db.authenticate(config.DB_USER, config.DB_PASS)
-            tests = db['test']
-
-            event['recipients'] = [i['email'] for i in tests.find(event['query'])]
+            queried = read_info(event, context)
+            if queried['statusCode'] != 200:
+                return queried
+            event['recipients'] = [i['email'] for i in queried['body'] if 'email' in i]
             if len(event['recipients']) == 0:
-                return config.add_cors_headers({'statusCode': 204, 'body': "No emails to send."})
-
+                return config.add_cors_headers({'statusCode': 204, 'body': "No recipients found!"})
         try:
             templ = emails.templates.get(event['template'])
             resp = emails.transmissions.send(
