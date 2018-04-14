ced150616fb05c65b7a79fa491a0f127456bf665
Heman <hemangandhi@gmail.com>
2018-04-14 14:03:47 -0400

    ignore some pyc, cal announce works

--- cal_announce.py
+++ cal_announce.py
@@ -7,35 +7,36 @@ import datetime
 
 #from oauth2client import tools
 from oauth2client.file import Storage
+from oauth2client import tools, client
 from googleapiclient import discovery
 
 import requests
 import json
+import config
 
-flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar Python API Quickstart'
 
-def google_cal(event, context):
+def google_cal(event, context, testing=False):
     num_events = event.get('num_events', 10)
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.google_credential_dir')
-    credential_path = os.path.join(credential_dir, 'calendar-python-quickstart.json')
+    credential_path = 'calendar-python-quickstart.json'
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
-        return config.add_cors_headers({'statusCode': 400, 'body': 'Please interactively generate client secret file.'})
-    #    flow = oauth2client.client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-    #    flow.user_agent = APPLICATION_NAME
-    #    credentials = tools.run_flow(flow, store, flags)
-    #    print("storing credentials to {}".format(credential_path))
+        if not testing:
+            return config.add_cors_headers({'statusCode': 400, 'body': 'Please interactively generate client secret file.'})
+        else:
+            flow = oauth2client.client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            credentials = tools.run_flow(flow, store)
+            print("storing credentials to {}".format(credential_path))
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
     now = datetime.datetime.utcnow().isoformat() + 'Z'
     print("getting the next 10 events")
     eventsResult = service.events().list(
-            calendarId = 'hl4bsn6030jr76nql68cen2jto', timeMin = now, maxResults = num_events * 5,
+            calendarId = 'hl4bsn6030jr76nql68cen2jto@group.calendar.google.com', timeMin = now, maxResults = num_events * 5,
             singleEvents = True, orderBy = 'startTime').execute()
     events = eventsResult.get('items', [])
     #if not events:
@@ -60,5 +61,5 @@ def slack_announce(event, context):
     if not allMessages:
         return config.add_cors_headers({'statusCode': 400, 'body': 'No messages found.'})
     messages = list(filter(lambda x: x.get('subtype') != 'channel_join', allMessages))
-    tenMessages = messages[:num_events]
+    tenMessages = messages[:num_messages]
     return config.add_cors_headers({'statusCode': 200, 'body': tenMessages})
