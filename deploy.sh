090a321f556a6f0397fa9fa1adba06002ba472ef
hemangandhi <hemangandhi@gmail.com>
2018-08-03 14:38:30 -0400

    deploy script not mac

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ pip install -r requirements.txt
 mkdir deploy/
 
 if [ -d "venv/lib/python3.6" ]; then
-    cp -r venv/lib/python3.6/site-packages/ deploy/
+    cp -r venv/lib/python3.6/site-packages/* deploy/
 else
-    cp -r venv/lib/python3.5/site-packages/ deploy/
+    cp -r venv/lib/python3.5/site-packages/* deploy/
 fi
 
 for var in "$@"
