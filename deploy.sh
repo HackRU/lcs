924b3120356be498780261e0acbf9e2e6f6f2c5d
hemangandhi <hemangandhi@gmail.com>
2018-08-04 17:43:14 -0400

    fixes for deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 mkdir deploy/
 
 if [ -d "venv/lib/python3.6" ]; then
-    cp -r venv/lib/python3.6/site-packages/* deploy/
+    cp -r venv/lib/python3.6/site-packages/. deploy/
 else
     cp -r venv/lib/python3.5/site-packages/* deploy/
 fi
@@ -15,7 +15,7 @@ do
 done
 cp config.py deploy/
 cd deploy/
-zip -r dep.zip *
+zip -r dep.zip .
 cd ..
 cp deploy/dep.zip .
 rm -rf deploy/
