d7a46c124d231e9979a45ab5abb03e0f65050cca
hemangandhi <hemangandhi@gmail.com>
2018-08-20 21:47:28 -0400

    new test file

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 # 1) Now you don't always have to copy paste the damn URL
 # 2) Deploys go sideways from within a venv, I think.
 
-source bin/activate
+source venv/bin/activate
+cp config.test.py config.py
 python tester.py https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/test
+rm config.py
 deactivate
