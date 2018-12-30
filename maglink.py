6b561c99ebbab6b450f2c1621da6ee99963bd1d6
Kabir Kuriyan <thekabistro@users.noreply.github.com>
2018-12-30 18:39:24 -0500

    Update maglink.py

--- maglink.py
+++ maglink.py
@@ -15,7 +15,7 @@ def forgotUser(event,magiclinks):
     obj_to_insert['forgot'] = True
     obj_to_insert[ "valid_until"] = (datetime.now() + timedelta(hours=3)).isoformat()
     magiclinks.insert_one(obj_to_insert)
-    link_base = event.get('link_base', 'https://hackru.org/magic/magiclink=')
+    link_base = event.get('link_base', 'https://hackru.org/magic/')
     rv = use_sparkpost.send_email(event['email'], link_base + magiclink,True)
     if rv['statusCode'] != 200:
         return rv
