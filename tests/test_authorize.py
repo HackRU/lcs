c9af0c0283bf081110072a98fcabe56f25fac54d
hemangandhi <hemangandhi@gmail.com>
2019-01-03 13:04:14 -0500

    fix auth merge issues

--- tests/test_authorize.py
+++ tests/test_authorize.py
@@ -28,12 +28,24 @@ def test_bad_password():
 
 @pytest.mark.run(order=1)
 def test_creation():
+    now = d.datetime.now(d.timezone.utc)
+    idx_of_time = list(
+            i for i in enumerate(config.REGISTRATION_DATES + [d.datetime.now(d.timezone.utc)]) \
+            if i[1] >= now)[0][0]
+    assert (not config.is_registration_open()) == idx_of_time % 2
+
+    #open registration
+    if not config.is_registration_open():
+        config.REGISTRATION_DATES = []
+    assert config.is_registration_open()
+
     user_email = "creep@radiohead.ed"
     passwd = "love"
     usr_dict = {'email': user_email, 'password': passwd}
     auth = authorize.create_user(usr_dict, None)
     assert dict_includes(auth, http_dict_for_token(user_email))
 
+@pytest.mark.run(order=2)
 def test_creation_fail_cases():
     user_email = "creep@radiohead.ed"
     passwd = "love"
@@ -44,6 +56,10 @@ def test_creation_fail_cases():
     usr_dict = {'email': user_email}
     auth = authorize.create_user(usr_dict, None)
     assert dict_includes(auth, http_dict(statusCode = 400, body = "No password provided"))
+
+    usr_dict = {'email': user_email, 'password': passwd}
+    auth = authorize.create_user(usr_dict, None)
+    assert dict_includes(auth, http_dict(statusCode = 400, body = "Duplicate user!"))
 
 @pytest.mark.run(order=2)
 def test_login_success():
