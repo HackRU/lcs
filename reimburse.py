d588bd2280ee227475934bf3a94816c3118de19d
hemangandhi <hemangandhi@gmail.com>
2019-02-13 14:04:40 -0500

    fix reimburse

--- reimburse.py
+++ reimburse.py
@@ -1,37 +1,56 @@
-import googlemaps
+import requests as req
 from pymongo import MongoClient
 
 import config
 from schemas import *
 
-def do_one_user(user, gmaps, db_conn):
-    # start_loc = gmaps.geocode(user['travelling_from']['formatted_addr'])[0]['geometry']['location']
-    start_loc = user['travelling_from']['formatted_addr']
-    rac_address = config.TRAVEL.HACKRU_LOCATION
-    mode = user['travelling_from']['mode']
+def req_matrix_and_clean(params):
+    def elem_to_dist(elem):
+        if elem['status'] != 'OK':
+            return 0
+        else:
+            return elem['distance']['value']
 
-    if mode not in ['car', 'plane']:
-        values = gmaps.distance_matrix(
-                origins = start_loc,
-                destinations = rac_address,
-                units = 'imperial',
-                mode = 'transit',
-                transit_mode = mode)['rows'][0]['elements'][0]['distance']['value']
-    elif mode == 'plane':
-        values = config.TRAVEL.MAX_REIMBURSE
-    else:
-        values = gmaps.distance_matrix(
-            origins = start_loc,
-            destinations = rac_address,
-            units = 'imperial',
-            mode = 'driving',
-            traffic_model = 'best_guess')
+    MATRIX_BASE_URL = "https://maps.googleapis.com/maps/api/distancematrix/json"
+    got = req.get(MATRIX_BASE_URL, params=params)
+    got.raise_for_status()
+    mat = got.json()
+    if mat['status'] != 'OK':
+        raise ValueError(mat)
 
-    values *= config.TRAVEL.MULTIPLIERS[mode]
-    values = min(values, config.TRAVEL.MAX_REIMBURSE)
+    return {val: elem_to_dist(mat['rows'][idx]['elements'][0]) for idx, val in enumerate(mat['origin_addresses'])}
 
-    db_conn.update_one({"email": user['email']}, {"$set": {"travelling_from.reimbursement": values}})
 
+def req_distance_matrices(users):
+    origins = "|".join(u['travelling_from']['formatted_addr'] for u in users)
+    destinations = config.TRAVEL.HACKRU_LOCATION
+
+    car_params = {
+        "origins": origins,
+        "destinations": destinations,
+        "mode": "driving",
+        "key": config.MAPS_API_KEY,
+    }
+    train_params = {
+        "origins": origins,
+        "destinations": destinations,
+        "mode": "transit",
+        "transit_mode": "train",
+        "key": config.MAPS_API_KEY,
+    }
+    bus_params = {
+        "origins": origins,
+        "destinations": destinations,
+        "mode": "transit",
+        "transit_mode": "bus",
+        "key": config.MAPS_API_KEY,
+    }
+
+    return {
+        "car": req_matrix_and_clean(car_params),
+        "bus": req_matrix_and_clean(bus_params),
+        "train": req_matrix_and_clean(train_params)
+    }
 
 @ensure_schema({
     "type": "object",
@@ -49,10 +68,19 @@ def compute_all_reimburse(event, context, user):
     db.authenticate(config.DB_USER,config.DB_PASS)
     tests = db[config.DB_COLLECTIONS['users']]
 
-    gmaps = googlemaps.Client(key=config.MAPS_API_KEY)
+    users = tests.find({"travelling_from": {"$exists": True}, "travelling_from.addr_ready": True})
+    try:
+        lookup = req_distance_matrices(users)
+    except Exception as e:
+        return {'statusCode': 512, 'body': repr(e)}
 
-    for user in tests.find({"travelling_from": {"$exists": True}, "travelling_from.addr_ready": True}):
-        do_one_user(user, gmaps, tests)
+    for user in users:
+        if user['travelling_from']['mode'] != 'plane':
+            dist = lookup[user['travelling_from']['mode']][user['travelling_from']['formatted_addr']]
+            reimburse = min(dist * config.TRAVEL.MULTIPLIERS[user['travelling_from']['mode']], config.TRAVEL.MAX_REIMBURSE)
+        else:
+            reimburse = config.TRAVEL.MAX_REIMBURSE
+        tests.update_one({"email": user['email']}, {"$set": {"travelling_from.reimbursement": reimburse}})
 
     return {'statusCode': 200, 'body': 'yote'}
 
