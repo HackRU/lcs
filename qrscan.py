71f640f52abff00735abfff9923ae0a9cc4b0047
mjrb <www.w.com123@gmail.com>
2019-09-21 13:03:54 -0400

    temporarily use tabs and fix errors found in testing

--- qrscan.py
+++ qrscan.py
@@ -1,5 +1,5 @@
 import json
-from schemas import ensure_schema, ensure_logged_in_user
+from schemas import ensure_schema, ensure_logged_in_user, ensure_role
 import pymongo
 from config import *
 from util import *
@@ -48,42 +48,43 @@ def qrMatch(event, context, user=None):
 		}
 
 @ensure_schema({
-    'type': 'object',
-    'properties': {
-        'auth_email': {'type': 'string', 'format': 'email'},
-        'token': {'type': 'string'},
-        'qr': {'type': 'string'},
-        'event': {'type': 'string'},
-        'again': {'type': 'boolean'}
-    },
-    'required': ['auth_email', 'token', 'qr', 'event']
+	'type': 'object',
+	'properties': {
+		'auth_email': {'type': 'string', 'format': 'email'},
+		'token': {'type': 'string'},
+		'qr': {'type': 'string'},
+		'event': {'type': 'string'},
+		'again': {'type': 'boolean'}
+	},
+	'required': ['auth_email', 'token', 'qr', 'event']
 })
 @ensure_logged_in_user(email_key='auth_email')
 @ensure_role([['director', 'organizer', 'volunteer']])
 def attend_event(aws_event, context, user=None):
-    users = util.coll('users')
-    qr = aws_event['qr']
-    event = aws_event['event']
-    again = aws_event.get('again', False)
+	# TODO fix indentation and whitespace
+	users = coll('users')
+	qr = aws_event['qr']
+	event = aws_event['event']
+	again = aws_event.get('again', False)
 
-    def attend(user):
-        if not again and user['day_of'][event] > 0:
-            return {'statusCode': 402, 'body': 'user already checked into event'}
-        new_user = users.find_one_and_update({'email': user['email']},
-                                             {'$inc': {'day_of': {event: 1}}},
-                                             return_document=ReturnDocument.AFTER)
+	def attend(user):
+		if not again and user['day_of'].get(event, 0) > 0:
+			return {'statusCode': 402, 'body': 'user already checked into event'}
+		new_user = users.find_one_and_update({'email': user['email']},
+						     {'$inc': {'day_of.' + event: 1}},
+						     return_document=pymongo.ReturnDocument.AFTER)
 
-        return {'statusCode': 200, 'body': {
-            'email': user['email'],
-            'new_count': new_user['day_of'][event]
-        }}
+		return {'statusCode': 200, 'body': {
+			'email': user['email'],
+			'new_count': new_user['day_of'][event]
+		}}
 
-    user = users.find_one({'email': qr})
-    if user:
-        attend(user)
+	user = users.find_one({'email': qr})
+	if user:
+		return attend(user)
 
-    user = users.find_one({'qrcode': qr})
-    if user:
-        attend(user)
+	user = users.find_one({'qrcode': qr})
+	if user:
+		return attend(user)
 
-    return {'statusCode': 404, 'body': 'user not found'}
+	return {'statusCode': 404, 'body': 'user not found'}
