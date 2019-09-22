bad03bdb29ad94c7462dcebc4b3645f84bb29985
Mickey J Winters <www.w.com123@gmail.com>
2019-09-22 09:36:58 -0400

    Event attend (#79)
    * add attend function
    
    * move event_attend to qrscan
    
    * reopen registration?
    
    * temporarily use tabs and fix errors found in testing
    
    * move attend event into qrcode.py
    
    * reorg imports, fix minor setup bug, add attend test
    
    * dont need that import

--- tests/test_qr_matching.py
+++ tests/test_qr_matching.py
@@ -1,12 +1,15 @@
-from testing_utils import *
-from pymongo import MongoClient
-import qrscan
-import config
-import validate
-import authorize
-import pytest
 import json
+
+from pymongo import MongoClient
+import pytest
 import requests
+
+import authorize
+import config
+import qrscan
+from testing_utils import *
+import util
+import validate
 
 email = "organizer@email.com"
 pwrord = "1234567"
@@ -22,7 +25,7 @@ def payload(auth_email=email):
 	return {"auth_email" : auth_email,
 		"token" : token,
 		"email" : hckemail,
-		"qr_code" : qr} 
+		"qr_code" : qr}
 
 def setup_module(m):
 	global old_col 
@@ -41,6 +44,7 @@ def setup_module(m):
 	
 	result = authorize.create_user({"email": hckemail, "password": hckword}, {})
 	assert result["statusCode"] == 200
+	global hcktoken
 	hcktoken = result["body"]["auth"]["token"]
 	
 def teardown_module(m):
@@ -60,3 +64,45 @@ def test_qr_match():
 	db = util.coll("users")
 	assert db.find_one({"email" : hckemail})["qrcode"][0] == qr
 
+def test_attend():
+	# TODO fix indentation and whitespace
+	event = 'free_supreme_bricks'
+	def attend(qr_code, again=False, auth_email=email, token=token):
+		return qrscan.attend_event({
+			'auth_email': auth_email,
+			'token': token,
+			'qr': qr_code,
+			'again': again,
+			'event': event
+		},{})
+
+	def test(qr_code):
+		# once
+		result = attend(qr_code)
+		assert result['statusCode'] == 200
+		# twice fail because again was not allowed
+		result = attend(qr_code)
+		assert result['statusCode'] == 402
+		# twice pass with again
+		result = attend(qr_code, again=True)
+		assert result['statusCode'] == 200
+
+	# by email
+	test(hckemail)
+
+	# reset day_of
+	users = util.coll('users')
+	users.update_one({'email': hckemail}, {'$set': {'day_of.' + event: 0}})
+
+	# by qr
+	result = qrscan.qrMatch(payload(), {})
+	assert result['statusCode'] == 200
+	test(qr)
+
+	# bad qr
+	result = attend('this_inst_an_email_or_qr@gmail.com')
+	assert result['statusCode'] == 404
+
+	#bad privs
+	result = attend(email, auth_email=hckemail, token=hcktoken)
+	assert result['statusCode'] == 403
