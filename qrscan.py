8223d0fdd24282d42a93d1ae95bd061509f6f2c3
mjrb <www.w.com123@gmail.com>
2019-09-23 20:33:18 -0400

    fix spacing

--- qrscan.py
+++ qrscan.py
@@ -5,86 +5,85 @@ from config import *
 from util import *
 
 qr_input = {
-	"type" : "object",
-	"Properties" : {
-		"auth_email" : {"type" : "string"},
-		"token" : {"type" : "string"},
-		"email" : {"type" : "string"},
-		"qr_code" : {"type" : "string"}
-	},
-	"required" : ["auth_email", "token", "email", "qr_code"]
+    "type" : "object",
+    "Properties" : {
+        "auth_email" : {"type" : "string"},
+	"token" : {"type" : "string"},
+	"email" : {"type" : "string"},
+	"qr_code" : {"type" : "string"}
+    },
+    "required" : ["auth_email", "token", "email", "qr_code"]
 }
 
 def dbinfo():
-	collection = coll("users")
-	print("collection name: " + str(collection.name))
-	print("collection database: " + str(collection.database))
-	print("collection document count: " + str(collection.count_documents))
+    collection = coll("users")
+    print("collection name: " + str(collection.name))
+    print("collection database: " + str(collection.database))
+    print("collection document count: " + str(collection.count_documents))
 
 @ensure_schema(qr_input)
 @ensure_logged_in_user(email_key = "auth_email")
 def qrMatch(event, context, user=None):
-	collection = coll('users')
-	
-	#User the user input to check for the position. Make sure its director and organizer	
-	if not user["role"]["organizer"]:
-		return {
-			"statusCode" : 400, 
-			"body" : "Permission Denied.  Not a organizer or director"
-		}
+    collection = coll('users')
 
-	result = collection.update_one({'email' : event["email"]}, {'$push' : {'qrcode' : event["qr_code"]}})
-	if result.matched_count == 1: 
-		return {
-			"statusCode" : 200, 
-			"body" : 
-				{"success" : True}
-		}
-	#{"status": db.update({'email' : email}, {'$push' : {'qrcode' : qr})
-	else:
-		return {
-			"statusCode" : 500,
-			"body" : "failed to upload QR into database"
-		}
+    #User the user input to check for the position. Make sure its director and organizer
+    if not user["role"]["organizer"]:
+        return {
+            "statusCode" : 400,
+            "body" : "Permission Denied.  Not a organizer or director"
+        }
+
+    result = collection.update_one({'email' : event["email"]}, {'$push' : {'qrcode' : event["qr_code"]}})
+    if result.matched_count == 1:
+        return {
+            "statusCode" : 200,
+            "body" : {"success" : True}
+        }
+        #{"status": db.update({'email' : email}, {'$push' : {'qrcode' : qr})
+    else:
+        return {
+            "statusCode" : 500,
+            "body" : "failed to upload QR into database"
+        }
 
 @ensure_schema({
-	'type': 'object',
-	'properties': {
-		'auth_email': {'type': 'string', 'format': 'email'},
-		'token': {'type': 'string'},
-		'qr': {'type': 'string'},
-		'event': {'type': 'string'},
-		'again': {'type': 'boolean'}
-	},
-	'required': ['auth_email', 'token', 'qr', 'event']
+    'type': 'object',
+    'properties': {
+        'auth_email': {'type': 'string', 'format': 'email'},
+        'token': {'type': 'string'},
+        'qr': {'type': 'string'},
+        'event': {'type': 'string'},
+        'again': {'type': 'boolean'}
+    },
+    'required': ['auth_email', 'token', 'qr', 'event']
 })
 @ensure_logged_in_user(email_key='auth_email')
 @ensure_role([['director', 'organizer', 'volunteer']])
 def attend_event(aws_event, context, user=None):
-	# TODO fix indentation and whitespace
-	users = coll('users')
-	qr = aws_event['qr']
-	event = aws_event['event']
-	again = aws_event.get('again', False)
+    # TODO fix indentation and whitespace
+    users = coll('users')
+    qr = aws_event['qr']
+    event = aws_event['event']
+    again = aws_event.get('again', False)
 
-	def attend(user):
-		if not again and user['day_of'].get(event, 0) > 0:
-			return {'statusCode': 402, 'body': 'user already checked into event'}
-		new_user = users.find_one_and_update({'email': user['email']},
-						     {'$inc': {'day_of.' + event: 1}},
-						     return_document=pymongo.ReturnDocument.AFTER)
+    def attend(user):
+        if not again and user['day_of'].get(event, 0) > 0:
+            return {'statusCode': 402, 'body': 'user already checked into event'}
+        new_user = users.find_one_and_update({'email': user['email']},
+                                             {'$inc': {'day_of.' + event: 1}},
+                                             return_document=pymongo.ReturnDocument.AFTER)
 
-		return {'statusCode': 200, 'body': {
-			'email': user['email'],
-			'new_count': new_user['day_of'][event]
-		}}
+        return {'statusCode': 200, 'body': {
+            'email': user['email'],
+            'new_count': new_user['day_of'][event]
+        }}
 
-	user = users.find_one({'email': qr})
-	if user:
-		return attend(user)
+    user = users.find_one({'email': qr})
+    if user:
+        return attend(user)
 
-	user = users.find_one({'qrcode': qr})
-	if user:
-		return attend(user)
+    user = users.find_one({'qrcode': qr})
+    if user:
+        return attend(user)
 
-	return {'statusCode': 404, 'body': 'user not found'}
+    return {'statusCode': 404, 'body': 'user not found'}
--- tests/test_qr_matching.py
+++ tests/test_qr_matching.py
@@ -22,87 +22,87 @@ hckword = "123456"
 hcktoken = ""
 
 def payload(auth_email=email):
-	return {"auth_email" : auth_email,
-		"token" : token,
-		"email" : hckemail,
-		"qr_code" : qr}
+    return {"auth_email" : auth_email,
+            "token" : token,
+            "email" : hckemail,
+            "qr_code" : qr}
 
 def setup_module(m):
-	global old_col 
-	old_col = config.DB_COLLECTIONS["users"]
-	config.DB_COLLECTIONS["users"] = "test-users"
-	#db = util.get_db()
-	#db["test-users"]
-		
-	result = authorize.create_user({"email": email, "password": pwrord}, {})
-	assert result["statusCode"] == 200
-	global token
-	token = result["body"]["auth"]["token"]
-	db = util.coll("users")
-	updete = db.update_one({"email":email}, {"$set" : { "role" : { "hacker" : False , "organizer" : True}}})
-	assert updete.modified_count >= 1
-	
-	result = authorize.create_user({"email": hckemail, "password": hckword}, {})
-	assert result["statusCode"] == 200
-	global hcktoken
-	hcktoken = result["body"]["auth"]["token"]
-	
-def teardown_module(m):
-	db = util.get_db()
-	db[config.DB_COLLECTIONS["users"]].drop()
+    global old_col
+    old_col = config.DB_COLLECTIONS["users"]
+    config.DB_COLLECTIONS["users"] = "test-users"
+    #db = util.get_db()
+    #db["test-users"]
 
-	global old_col
-	config.DB_COLLECTIONS["users"] = old_col
+    result = authorize.create_user({"email": email, "password": pwrord}, {})
+    assert result["statusCode"] == 200
+    global token
+    token = result["body"]["auth"]["token"]
+    db = util.coll("users")
+    updete = db.update_one({"email":email}, {"$set" : { "role" : { "hacker" : False , "organizer" : True}}})
+    assert updete.modified_count >= 1
+
+    result = authorize.create_user({"email": hckemail, "password": hckword}, {})
+    assert result["statusCode"] == 200
+    global hcktoken
+    hcktoken = result["body"]["auth"]["token"]
+
+def teardown_module(m):
+    db = util.get_db()
+    db[config.DB_COLLECTIONS["users"]].drop()
+
+    global old_col
+    config.DB_COLLECTIONS["users"] = old_col
 
 def test_bad_role():
-	result = qrscan.qrMatch(payload(auth_email=hckemail), {})
-	assert result["statusCode"] == 400
+    result = qrscan.qrMatch(payload(auth_email=hckemail), {})
+    assert result["statusCode"] == 400
 
 def test_qr_match():
-	result = qrscan.qrMatch(payload(), {})
-	assert result["statusCode"] == 200
-	db = util.coll("users")
-	assert db.find_one({"email" : hckemail})["qrcode"][0] == qr
+    result = qrscan.qrMatch(payload(), {})
+    assert result["statusCode"] == 200
+    db = util.coll("users")
+    assert db.find_one({"email" : hckemail})["qrcode"][0] == qr
 
 def test_attend():
-	# TODO fix indentation and whitespace
-	event = 'free_supreme_bricks'
-	def attend(qr_code, again=False, auth_email=email, token=token):
-		return qrscan.attend_event({
-			'auth_email': auth_email,
-			'token': token,
-			'qr': qr_code,
-			'again': again,
-			'event': event
-		},{})
+    # TODO fix indentation and whitespace
+    event = 'free_supreme_bricks'
+    def attend(qr_code, again=False, auth_email=email, token=token):
+        return qrscan.attend_event({
+            'auth_email': auth_email,
+            'token': token,
+            'qr': qr_code,
+            'again': again,
+            'event': event
+        },{})
 
-	def test(qr_code):
-		# once
-		result = attend(qr_code)
-		assert result['statusCode'] == 200
-		# twice fail because again was not allowed
-		result = attend(qr_code)
-		assert result['statusCode'] == 402
-		# twice pass with again
-		result = attend(qr_code, again=True)
-		assert result['statusCode'] == 200
+    def test(qr_code):
+        # once
+        result = attend(qr_code)
+        assert result['statusCode'] == 200
+        # twice fail because again was not allowed
+        result = attend(qr_code)
+        assert result['statusCode'] == 402
+        # twice pass with again
+        result = attend(qr_code, again=True)
+        assert result['statusCode'] == 200
 
-	# by email
-	test(hckemail)
+    # by email
+    test(hckemail)
 
-	# reset day_of
-	users = util.coll('users')
-	users.update_one({'email': hckemail}, {'$set': {'day_of.' + event: 0}})
+    # reset day_of
+    users = util.coll('users')
+    users.update_one({'email': hckemail}, {'$set': {'day_of.' + event: 0}})
 
-	# by qr
-	result = qrscan.qrMatch(payload(), {})
-	assert result['statusCode'] == 200
-	test(qr)
+    # by qr
+    result = qrscan.qrMatch(payload(), {})
+    assert result['statusCode'] == 200
+    test(qr)
 
-	# bad qr
-	result = attend('this_inst_an_email_or_qr@gmail.com')
-	assert result['statusCode'] == 404
+    # bad qr
+    result = attend('this_inst_an_email_or_qr@gmail.com')
+    assert result['statusCode'] == 404
 
-	#bad privs
-	result = attend(email, auth_email=hckemail, token=hcktoken)
-	assert result['statusCode'] == 403
+    #bad privs
+    result = attend(email, auth_email=hckemail, token=hcktoken)
+    assert result['statusCode'] == 403
