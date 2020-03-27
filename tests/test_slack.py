239455bfc19439d14b6e4434edab09997d62c61e
Anarav <10067468+Anarav@users.noreply.github.com>
2020-03-27 15:42:16 -0400

    Add comments

--- tests/test_slack.py
+++ tests/test_slack.py
@@ -3,34 +3,44 @@ import config
 import util
 from slack import generate_dm_link
 
+# credentials of the generator
 email = "generator@test.com"
 password = "temp_password"
 token = None
 slack_id = "U010WR7TMAQ"
+
+# credentials of the other user
 other_email = "other@test.com"
 other_password = "secure_password"
 other_slack_id = "U010WRFRJ7N"
+
+# collections and database
 old_col = None
 db = None
 
 payload = None
 
 
+# helper method to unset the slack id in the db document identified by slack_email
 def unset_slack_id(slack_email):
     update = db.update_one({"email": slack_email}, {"$unset": {"slack_id": 1}})
     assert update.modified_count >= 1
 
 
+# helper method to set the slack id to the slack_id_to_set in the db document identified by email_to_set
 def set_slack_id(email_to_set, slack_id_to_set):
     update = db.update_one({"email": email_to_set}, {"$set": {"slack_id": slack_id_to_set}})
     assert update.modified_count >= 1
 
 
+# method that sets up the module in preparation for running the tests
 def setup_module(m):
+    # import globals and switch the collection to a temporary collection
     global old_col, token, db, payload
     old_col = config.DB_COLLECTIONS["users"]
     config.DB_COLLECTIONS["users"] = "test-users"
 
+    # create a dummy user
     result = authorize.create_user({"email": email, "password": password}, {})
     assert result["statusCode"] == 200
     token = result["body"]["auth"]["token"]
@@ -40,16 +50,20 @@ def setup_module(m):
         "other_email": other_email
     }
 
+    # create another dummy user
     result = authorize.create_user({"email": other_email, "password": other_password}, {})
     assert result["statusCode"] == 200
     assert bool(config.SLACK_KEYS["token"])
 
+    # sets the slack id in the db documents
     db = util.coll("users")
     set_slack_id(email, slack_id)
     set_slack_id(other_email, other_slack_id)
 
 
+# tears down a module
 def teardown_module(m):
+    # drops the temp collection that was created
     global db, old_col
     db = util.get_db()
     db[config.DB_COLLECTIONS["users"]].drop()
@@ -92,14 +106,12 @@ def test_missing_slack_id():
 
 
 def test_invalid_slack_id():
-    update = db.update_one({"email": email}, {"$set": {"slack_id": "bad slack id"}})
-    assert update.modified_count >= 1
+    set_slack_id(email, "bad slack id")
     response = generate_dm_link(payload, {})
     assert response["statusCode"] == 503
     set_slack_id(email, slack_id)
 
-    update = db.update_one({"email": other_email}, {"$set": {"slack_id": "bad slack id"}})
-    assert update.modified_count >= 1
+    set_slack_id(other_email, "bad slack id")
     response = generate_dm_link(payload, {})
     assert response["statusCode"] == 503
     set_slack_id(other_email, other_slack_id)
