db6f067a26db20e27980d55d1e0110b761a160cf
Anarav <10067468+Anarav@users.noreply.github.com>
2020-04-02 19:58:08 -0400

    Slack dm endpoint (#97)
    * Implement slack dm link generation
    
    * Implement tests
    
    * Add comments
    
    * Fix linter error
    
    * Fetch slack api key from environment variable
    
    * Match envionment variable with Travis CI

---
+++ slack.py
@@ -0,0 +1,68 @@
+import requests
+
+import config
+import util
+from schemas import ensure_schema, ensure_logged_in_user
+from util import add_cors_headers
+
+
+def create_error_response(err_msg: str):
+    return add_cors_headers({"statusCode": 503, "body": err_msg})
+
+
+def process_slack_error(error_str: str):
+    if error_str in ["user_not_found", "user_not_visible", "user_disabled"]:
+        return add_cors_headers(
+                {"statusCode": 403, "body": f"There was an error with the user id's provided: {error_str}"})
+    else:
+        return create_error_response(f"Encountered a slack API error: {error_str}")
+
+
+@ensure_schema({
+    "type": "object",
+    "properties": {
+        "email": {"type": "string", "format": "email"},
+        "token": {"type": "string"},
+        "other_email": {"type": "string", "format": "email"}
+    },
+    "required": ["email", "token", "other_email"]
+})
+@ensure_logged_in_user() # makes sure that the requestor is a logged in user
+def generate_dm_link(event, context, user):
+    # attempts to find the other user based on the email provided
+    other_user = util.coll("users").find_one({"email": event["other_email"]})
+    # ensures that other user exists in LCS
+    if other_user is None:
+        return add_cors_headers({"statusCode": 403, "body": "Other user not found within LCS"})
+    # ensures Slack Token is present in the config
+    if "token" not in config.SLACK_KEYS or not config.SLACK_KEYS["token"]:
+        return create_error_response("Slack API token not configured")
+    # fetches the slack id from their LCS profile
+    this_slack_id = user.get("slack_id", None)
+    # fetches the other user's slack id from their LCS profile
+    other_slack_id = other_user.get("slack_id", None)
+    # ensures both id's exist
+    if this_slack_id is None or other_slack_id is None:
+        return add_cors_headers({"statusCode": 403, "body": "Slack ID not present within LCS for the given user(s)"})
+    # creates the link, payload and headers to make the request
+    api_link = r"https://slack.com/api/conversations.open"
+    slack_api_payload = {"token": config.SLACK_KEYS["token"], "users": f"{this_slack_id},{other_slack_id}"}
+    slack_api_headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    # calls the API to open a group dm
+    response = requests.post(url=api_link, data=slack_api_payload, headers=slack_api_headers)
+    # ensures there weren't any errors calling the API
+    if response.status_code != 200:
+        return create_error_response("Encountered a Slack API error")
+    # fetches the json and examines it to determine if it was successful
+    response_json = response.json()
+    was_successful = response_json["ok"]
+    # in case of failure, error message is attached and returned back
+    if not was_successful:
+        return process_slack_error(response_json["error"])
+    # if everything goes well, fetches the necessary id to create the link
+    creation_info = response_json["channel"]
+    dm_id = creation_info["id"]
+    server_id = creation_info["shared_team_ids"][0]
+    link_to_dm = f"https://apps.slack.com/client/{server_id}/{dm_id}"
+    # returns the link and OK status code
+    return add_cors_headers({"statusCode": 200, "body": {"slack_dm_link": link_to_dm}})
---
+++ tests/test_slack.py
@@ -0,0 +1,133 @@
+import os
+
+import authorize
+import config
+import util
+from slack import generate_dm_link
+
+# credentials of the generator
+email = "generator@test.com"
+password = "temp_password"
+token = None
+slack_id = "U010WR7TMAQ"
+
+# credentials of the other user
+other_email = "other@test.com"
+other_password = "secure_password"
+other_slack_id = "U010WRFRJ7N"
+
+# collections and database
+old_col = None
+db = None
+
+payload = None
+
+current_slack_key = ""
+
+
+# helper method to unset the slack id in the db document identified by slack_email
+def unset_slack_id(slack_email):
+    update = db.update_one({"email": slack_email}, {"$unset": {"slack_id": 1}})
+    assert update.modified_count >= 1
+
+
+# helper method to set the slack id to the slack_id_to_set in the db document identified by email_to_set
+def set_slack_id(email_to_set, slack_id_to_set):
+    update = db.update_one({"email": email_to_set}, {"$set": {"slack_id": slack_id_to_set}})
+    assert update.modified_count >= 1
+
+
+# method that sets up the module in preparation for running the tests
+def setup_module(m):
+    # import globals and switch the collection to a temporary collection
+    global old_col, token, db, payload, current_slack_key
+    old_col = config.DB_COLLECTIONS["users"]
+    config.DB_COLLECTIONS["users"] = "test-users"
+
+    # swap the key to be fetched from environment variable
+    current_slack_key = config.SLACK_KEYS["token"]
+    config.SLACK_KEYS["token"] = os.getenv("SLACK_API_TOKEN_BOT", "")
+
+    # create a dummy user
+    result = authorize.create_user({"email": email, "password": password}, {})
+    assert result["statusCode"] == 200
+    token = result["body"]["auth"]["token"]
+    payload = {
+        "email": email,
+        "token": token,
+        "other_email": other_email
+    }
+
+    # create another dummy user
+    result = authorize.create_user({"email": other_email, "password": other_password}, {})
+    assert result["statusCode"] == 200
+    assert bool(config.SLACK_KEYS["token"])
+
+    # sets the slack id in the db documents
+    db = util.coll("users")
+    set_slack_id(email, slack_id)
+    set_slack_id(other_email, other_slack_id)
+
+
+# tears down a module
+def teardown_module(m):
+    # drops the temp collection that was created
+    global db, old_col, current_slack_key
+    db = util.get_db()
+    db[config.DB_COLLECTIONS["users"]].drop()
+    config.DB_COLLECTIONS["users"] = old_col
+
+    # restore the slack api key
+    config.SLACK_KEYS["token"] = current_slack_key
+
+
+def test_bad_email():
+    response = generate_dm_link({"email": "invalid@email.com", "token": token, "other_email": other_email}, {})
+    assert response["statusCode"] == 403
+
+
+def test_bad_token():
+    response = generate_dm_link({"email": email, "token": "bad token", "other_email": other_email}, {})
+    assert response["statusCode"] == 403
+
+
+def test_bad_other_email():
+    response = generate_dm_link({"email": email, "token": token, "other_email": "invalid@email.com"}, {})
+    assert response["statusCode"] == 403
+
+
+def test_misconfigured_slack_token():
+    slack_token = config.SLACK_KEYS["token"]
+    config.SLACK_KEYS["token"] = "bad slack token"
+    response = generate_dm_link(payload, {})
+    assert response["statusCode"] == 503
+    config.SLACK_KEYS["token"] = slack_token
+
+
+def test_missing_slack_id():
+    unset_slack_id(email)
+    response = generate_dm_link(payload, {})
+    assert response["statusCode"] == 403
+    set_slack_id(email, slack_id)
+
+    unset_slack_id(other_email)
+    response = generate_dm_link(payload, {})
+    assert response["statusCode"] == 403
+    set_slack_id(other_email, other_slack_id)
+
+
+def test_invalid_slack_id():
+    set_slack_id(email, "bad slack id")
+    response = generate_dm_link(payload, {})
+    assert response["statusCode"] == 503
+    set_slack_id(email, slack_id)
+
+    set_slack_id(other_email, "bad slack id")
+    response = generate_dm_link(payload, {})
+    assert response["statusCode"] == 503
+    set_slack_id(other_email, other_slack_id)
+
+
+def test_valid_use():
+    response = generate_dm_link(payload, {})
+    assert response["statusCode"] == 200
