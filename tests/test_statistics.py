2ca1e500898e65427c2159483c89da7554e7adc3
Ethan Nguyen <en256@scarletmail.rutgers.edu>
2023-08-31 21:22:21 -0400

    test trial run

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -2,36 +2,36 @@ from testing_utils import *
 from src import statistics, authorize
 import pytest
 
-@pytest.mark.run(order=1)
-def test_when_director_logs_in(): # success case
-    user_email = "director@test"
-    password = "director"
-    usr_dict = {"email": user_email, "password": password}
-    auth = authorize.authorize(usr_dict, None)
+# @pytest.mark.run(order=1)
+# def test_when_director_logs_in(): # success case
+#     user_email = "director@test"
+#     password = "director"
+#     usr_dict = {"email": user_email, "password": password}
+#     auth = authorize.authorize(usr_dict, None)
 
-    auth_token = auth['body']['token']
+#     auth_token = auth['body']['token']
 
-    # make sure user exists
-    user_dict = get_db_user(user_email)
-    assert 'email' in user_dict and user_dict['email'] == user_email
+#     # make sure user exists
+#     user_dict = get_db_user(user_email)
+#     assert 'email' in user_dict and user_dict['email'] == user_email
 
-    # make call 
-    success = statistics.statistics({'token': auth_token}, None)
-    expected_schema = {
-        "type": "object",
-        "properties": {
-            "registered": {"type": "integer"},
-            "rejected": {"type": "integer"},
-            "confirmation": {"type": "integer"},
-            "coming": {"type": "integer"},
-            "not-coming": {"type": "integer"},
-            "waitlist": {"type": "integer"},
-            "confirmed": {"type": "integer"},
-            "checked-in": {"type": "integer"}
-        },
-        "required": ['registered', "rejected", "confirmation", "coming", "not-coming", "waitlist", "confirmed", "checked-in"]
-    }
-    assert check_by_schema(schema_for_http(200, expected_schema), success)
+#     # make call 
+#     success = statistics.statistics({'token': auth_token}, None)
+#     expected_schema = {
+#         "type": "object",
+#         "properties": {
+#             "registered": {"type": "integer"},
+#             "rejected": {"type": "integer"},
+#             "confirmation": {"type": "integer"},
+#             "coming": {"type": "integer"},
+#             "not-coming": {"type": "integer"},
+#             "waitlist": {"type": "integer"},
+#             "confirmed": {"type": "integer"},
+#             "checked-in": {"type": "integer"}
+#         },
+#         "required": ['registered', "rejected", "confirmation", "coming", "not-coming", "waitlist", "confirmed", "checked-in"]
+#     }
+#     assert check_by_schema(schema_for_http(200, expected_schema), success)
 
 @pytest.mark.run(order=2)
 def test_when_non_director_logs_in(): # failure case
